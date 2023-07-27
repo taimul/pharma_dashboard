6ddcb4de27221d130d872ca27dd0440cf83e4688
taimul_Pathan <taimul.haque@gmail.com>
2023-07-27 10:41:10 +0600

    initial setup done

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { RouterProvider } from "react-router-dom";
+import { router } from "./Routes/Router";
+
+function App() {
+  return (
+    <div>
+      <RouterProvider router={router}></RouterProvider>
+    </div>
+  );
+}
+
+export default App;
